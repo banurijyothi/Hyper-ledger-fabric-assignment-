92f97943e19ff85405002cab54ef7dfbfb6451f7
banurijyothi <134289534+banurijyothi@users.noreply.github.com>
2024-09-13 15:06:57 +0530

    Create test.go

---
+++ test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "testing"
+    "github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+func TestCreateAsset(t *testing.T) {
+    // Implement test logic to interact with the chaincode
+}
+
+func TestQueryAsset(t *testing.T) {
+    // Implement test logic to interact with the chaincode
+}
